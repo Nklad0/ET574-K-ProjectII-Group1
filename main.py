f10365fec58b1f97e6481bb6d33ed1f362189d2b
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-12-05 23:46:31 +0000

    Added matplotlib

--- main.py
+++ main.py
@@ -37,10 +37,34 @@ class WineQualityRate(wx.Frame):
                 return
             path = dlg.GetPath()
         
-        try:
-           df = pd.read_csv(path)
-           want = ["fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol"]
-           df.columns = [str(c).strip().lower().replace(" ", "_") for c in df.columns]
-           if df.shape[1] == 5 and not set(want).issubset(df.columns):
-               df.columns = want
+    try:
+        df = pd.read_csv(path)
+        want = ["fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol"]
+        df.columns = [str(c).strip().lower().replace(" ", "_") for c in df.columns]
+        if df.shape[1] == 5 and not set(want).issubset(df.columns):
+            df.columns = want
         
+        for c in ["fixed_acidity", "volatile_acidity", "quality"]:
+            if c not in df.columns:
+                raise ValueError("CSV needs to have 4 features + 'quality'.")
+            
+
+        plt.figure(figsize=(7.5, 5.0))
+        for sp, sub in df.groupby("quality"):
+            plt.bar(sub[X], sub[Y], s=40, alpha=0.85, label=str(sp))
+        plt.title(f"Wine Quality: {X.replace('_',' ').title()} vs {Y.replace('_',' ').title()}")
+        plt.xlabel(X.replace('_',' ').title())
+        plt.ylabel(Y.replace('_',' ').title())
+        plt.grid(True, linestyle="--", alpha=0.3)
+        plt.legend(title="quality", frameon=True)
+        plt.tight_layout()
+        plt.show()
+
+        
+    except Exception as e:
+              wx.MessageBox(f"Failed to load/plot file:\n{e}", "Error", wx.OK | wx.ICON_ERROR)
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    WineQualityRate()
+    app.MainLoop()
